ce5c9f943af7cbdb2365217ca020ad129dadbc32
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-05-16 17:06:52 -0300

    fondo movido

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://emiigonzalez.github.io/fines_com10/',
 })
